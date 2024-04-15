f437b6d56e2f4ce0180d19c7a2460726cb70681f
Onsraa <truongteddy306@icloud.com>
2024-04-15 10:49:45 +0200

    Add functions for minimap

--- Assets/Scripts/Cameras/CameraManager.cs
+++ Assets/Scripts/Cameras/CameraManager.cs
@@ -1,4 +1,5 @@
 using Cinemachine;
+using Maps.Classes;
 using UnityEngine;
 
 namespace Cameras
@@ -6,18 +7,27 @@ namespace Cameras
     public class CameraManager : MonoBehaviour
     {
         private CinemachineVirtualCamera _virtualCamera;
-    
+        private Minimap _minimap;
+
         public bool freeView = true;
     
         private Transform _selectedObjectTransform;
         private Transform _cameraSystemTransform;
-    
-        // Start is called before the first frame update
+        
         void Start()
         {
-            _virtualCamera = FindObjectOfType<CinemachineVirtualCamera>();    
+            //Find virtual camera
+            _virtualCamera = FindObjectOfType<CinemachineVirtualCamera>();   
+            
+            // Find camera system
             _cameraSystemTransform = FindObjectOfType<CameraSystem>().transform;
+            
+            // Initialize the first selected object as the camera system
             _selectedObjectTransform = _cameraSystemTransform;
+            
+            // Find minimap
+            _minimap = FindObjectOfType<Minimap>();
+            
             UpdateView();
         }
 
@@ -39,18 +49,27 @@ namespace Cameras
             
                 _virtualCamera.LookAt = _cameraSystemTransform;
                 _virtualCamera.Follow = _cameraSystemTransform;
+                
+                //Make the minimap the child of the camera system object
+                _minimap.RepositionCamera(_cameraSystemTransform);
+
             }
             else
             {
                 _virtualCamera.LookAt = _selectedObjectTransform;
-                _virtualCamera.Follow = _selectedObjectTransform;        
+                _virtualCamera.Follow = _selectedObjectTransform;   
+                                
+                //Make the minimap the child of the selected object
+                _minimap.RepositionCamera(_selectedObjectTransform);
             }
             freeView = !freeView;
         }
 
         public void UpdateLastSelectedObject(Transform selectedObject)
         {
-            _selectedObjectTransform = selectedObject;
+            _selectedObjectTransform = selectedObject; 
+             //Make the minimap the child of the selected object
+             _minimap.RepositionCamera(_selectedObjectTransform);
             freeView = true;
         }
     }
