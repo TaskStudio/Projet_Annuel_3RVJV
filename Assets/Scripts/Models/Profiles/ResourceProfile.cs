904dd3b9271ebbb44cd269712468d0d5af183541
Onsraa <truongteddy306@icloud.com>
2024-06-11 17:13:05 +0200

    Add profile model for resource (Scriptable object)

--- Assets/Scripts/Models/Profiles/ResourceProfile.cs
+++ Assets/Scripts/Models/Profiles/ResourceProfile.cs
@@ -1,10 +1,9 @@
 using UnityEngine;
 
-[System.Serializable]
-public class ResourceProfile : IProfile
+[CreateAssetMenu(fileName = "NewResourceProfile", menuName = "Profiles/ResourceProfile")]
+public class ResourceProfile : ScriptableObject, IProfile
 {
     public string Name { get; set; }
     public string Description { get; set; }
-    public Texture2D Image { get; set; }
-    public string ResourceType { get; set; }
+    public string ResourceType;
 }
