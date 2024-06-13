5f32ac4f3a3f5609785d901972cec24d6b3b83d3
MrSebi06 <mihneasebastian06@gmail.com>
2024-06-13 10:24:34 +0200

    Rename Gatherer/ to Gathering/

---
+++ Assets/Scripts/Models/Gathering/ResourceStorage.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceStorage : MonoBehaviour, IResourceStorage
+{
+    public Dictionary<ResourceNode.ResourceType, int> storedResources = new();
+
+    public void AddResource(ResourceNode.ResourceType type, int amount)
+    {
+        storedResources.TryAdd(type, 0);
+
+        storedResources[type] += amount;
+    }
+
+    public int GetResourceAmount(ResourceNode.ResourceType type)
+    {
+        return storedResources.GetValueOrDefault(type, 0);
+    }
+}
