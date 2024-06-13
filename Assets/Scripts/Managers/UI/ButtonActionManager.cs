86f597579f4ec597f649253c3a8619cc04d31f90
Onsraa <truongteddy306@icloud.com>
2024-06-13 22:05:42 +0200

    Delete comments

--- Assets/Scripts/Managers/UI/ButtonActionManager.cs
+++ Assets/Scripts/Managers/UI/ButtonActionManager.cs
@@ -13,16 +13,13 @@ public class ButtonActionManager : MonoBehaviour
 
     private void OnEnable()
     {
-        // Get the root VisualElement
         var root = mainUIDocument.rootVisualElement;
 
-        // Find buttons in the main UI document
         var startButton = root.Q<Button>("StartButton");
         var settingsButton = root.Q<Button>("SettingsButton");
         var creditButton = root.Q<Button>("CreditButton");
         var exitButton = root.Q<Button>("ExitButton");
 
-        // Add event listeners to each button
         startButton?.RegisterCallback<ClickEvent>(ev => OnStartButtonClick());
         settingsButton?.RegisterCallback<ClickEvent>(ev => OnSettingsButtonClick());
         creditButton?.RegisterCallback<ClickEvent>(ev => OnCreditButtonClick());
@@ -30,26 +27,25 @@ public class ButtonActionManager : MonoBehaviour
 
         settingsContainer = settingsDocument.rootVisualElement.Q<VisualElement>("Background");
 
-        // Ensure the settings and credits documents are initially hidden
         settingsDocument.rootVisualElement.style.display = DisplayStyle.None;
         creditsDocument.rootVisualElement.style.display = DisplayStyle.None;
     }
 
     private void OnStartButtonClick()
     {
-        SceneManager.LoadScene(nextScene); // Replace "MainScene" with your actual scene name
+        SceneManager.LoadScene(nextScene);
     }
 
     private void OnSettingsButtonClick()
     {
         settingsDocument.rootVisualElement.style.display = DisplayStyle.Flex;
-        mainUIDocument.rootVisualElement.style.display = DisplayStyle.None; // Hide the main menu
+        mainUIDocument.rootVisualElement.style.display = DisplayStyle.None; 
     }
 
     private void OnCreditButtonClick()
     {
         creditsDocument.rootVisualElement.style.display = DisplayStyle.Flex;
-        mainUIDocument.rootVisualElement.style.display = DisplayStyle.None; // Hide the main menu
+        mainUIDocument.rootVisualElement.style.display = DisplayStyle.None; 
     }
 
     private void OnExitButtonClick()
