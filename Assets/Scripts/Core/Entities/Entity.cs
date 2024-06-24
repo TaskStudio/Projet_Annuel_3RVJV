2ea13636faf8a0ad82b19a5612e624b21727a8a9
Onsraa <truongteddy306@icloud.com>
2024-06-24 16:16:05 +0200

    Add meta data

--- Assets/Scripts/Core/Entities/Entity.cs
+++ Assets/Scripts/Core/Entities/Entity.cs
@@ -6,17 +6,68 @@ public class Entity : BaseObject
 {
     [Space(10)] [Header("Actions")]
     public List<UnityEvent> actionList;
+    
+    public EntityData entityData;
+    public int currentHealth;
+    public int currentMana = 0;
+    public float attackSpeed = 1.0f;
+    public float movementSpeed = 0.5f;
+    public string race;
 
-    public int hp;
-    public int maxHp { get; }
+    private void Start()
+    {
+        Initialize(entityData);
+    }
+    
+    public override void Initialize(ObjectData data)
+    {
+        base.Initialize(data);
+        if (data is EntityData entityData)
+        {
+            this.entityData = entityData;
+            currentHealth = entityData.maxHealthPoints;
+            currentMana = entityData.maxManaPoints;
+            attackSpeed = entityData.attackSpeed;
+            movementSpeed = entityData.movementSpeed;
+            race = entityData.race;
+        }
+    }
 
     public void TakeDamage(int damage)
     {
-        hp -= damage;
-        if (hp < 0)
+        currentHealth -= damage;
+        if (currentHealth < 0)
         {
-            hp = 0;
-            gameObject.SetActive(false);
+            currentHealth = 0;
         }
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public int GetHealthPoints()
+    {
+        return currentHealth;
+    }
+    
+    public void SetHealthPoints(int currentHealthPoints)
+    {
+        currentHealth = currentHealthPoints;
+    }
+
+    public int GetMaxHealthPoints()
+    {
+        return entityData.maxHealthPoints;
+    }
+
+    public void SetMaxHealthPoints(int maxHealthPoints)
+    {
+        entityData.maxHealthPoints = maxHealthPoints;
+    }
+
+    protected virtual void Die()
+    {
+        gameObject.SetActive(false);
     }
 }
