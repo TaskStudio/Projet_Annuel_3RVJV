b61f473ccf29c2bb5052f2c36a24711ba4d0032e
Onsraa <truongteddy306@icloud.com>
2024-06-24 16:16:27 +0200

    Add meta data from scriptable object

--- Assets/Scripts/Core/BaseObject.cs
+++ Assets/Scripts/Core/BaseObject.cs
@@ -1,10 +1,32 @@
+using System;
 using UnityEngine;
 
 public abstract class BaseObject : MonoBehaviour
 {
+    public string objectName;
+    public string description;
+    public Texture2D image;
+    
     [Space(10)] [Header("Visuals")]
     [SerializeField] private GameObject model;
     public bool IsSelected { get; set; }
+
+    public virtual void Initialize(ObjectData data)
+    {
+        objectName = data.objectName;
+        description = data.description;
+        image = data.image;
+    }
+    
+    public string GetName()
+    {
+        return objectName;
+    }
+
+    public string GetDescription()
+    {
+        return description;
+    }
 
     public void Select()
     {
