1a3b59168544c0565b7702188bc6c1a1b480a8d7
MrSebi06 <mihneasebastian06@gmail.com>
2024-07-02 12:22:22 +0200

    Clean BaseObject

--- Assets/Scripts/Core/BaseObject.cs
+++ Assets/Scripts/Core/BaseObject.cs
@@ -1,51 +1,36 @@
-using System;
 using UnityEngine;
 
 public abstract class BaseObject : MonoBehaviour
 {
-    public string objectName;
-    public string description;
-    public Texture2D image;
     public ObjectData data;
-    
+
     [Space(10)] [Header("Visuals")]
     [SerializeField] private GameObject model;
-    public bool IsSelected { get; set; }
 
-    public virtual void Initialize()
-    {
-        objectName = data.objectName;
-        description = data.description;
-        image = data.image;
-    }
-    
-    public string GetName()
-    {
-        return objectName;
-    }
+    private bool isSelected;
 
-    public string GetDescription()
+    public bool IsSelected
     {
-        return description;
+        get => isSelected;
+        private set
+        {
+            isSelected = value;
+            UpdateVisuals();
+        }
     }
 
     public void Select()
     {
         IsSelected = true;
-        UpdateVisuals();
     }
 
     public void Deselect()
     {
         IsSelected = false;
-        UpdateVisuals();
     }
 
-    public void UpdateVisuals()
+    private void UpdateVisuals()
     {
-        if (IsSelected)
-            model.layer = LayerMask.NameToLayer("Outlined");
-        else
-            model.layer = LayerMask.NameToLayer("Default");
+        model.layer = LayerMask.NameToLayer(isSelected ? "Outlined" : "Default");
     }
 }
