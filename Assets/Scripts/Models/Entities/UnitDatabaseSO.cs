323403d36e77814ee7253d21adf21914ce4973dc
MrSebi06 <mihneasebastian06@gmail.com>
2024-07-12 16:46:36 +0200

    Rename & update databases

--- Assets/Scripts/Models/Entities/UnitDatabaseSO.cs
+++ Assets/Scripts/Models/Entities/UnitDatabaseSO.cs
@@ -5,36 +5,37 @@ using UnityEngine;
 namespace Entities
 {
     [CreateAssetMenu]
-    public class EntityDatabaseSO : ScriptableObject
+    public class UnitDatabaseSO : ScriptableObject
     {
-        public List<EntityGathererData> entitiesData;
+        public List<UnitDatabaseData> units;
 
-        public Unit GetEntityPrefab(string entityID)
+        public Unit GetUnitPrefab(string unitID)
         {
-            EntityGathererData entityData = entitiesData.Find(data => data.ID == entityID);
-            if (entityData == null)
+            UnitDatabaseData unitDatabaseData = units.Find(data => data.ID == unitID);
+            if (unitDatabaseData == null)
             {
-                Debug.LogError("Entity with ID " + entityID + " not found in database.");
+                Debug.LogError("Entity with ID " + unitID + " not found in database.");
                 return null;
             }
 
-            return entityData.Prefab;
+            return unitDatabaseData.Prefab;
         }
 
-        public EntityGathererData GetEntityData(string entityID)
+        public UnitDatabaseData GetEntityData(string entityID)
         {
-            return entitiesData.Find(data => data.ID == entityID);
+            return units.Find(data => data.ID == entityID);
         }
     }
 
     [Serializable]
-    public class EntityGathererData{
-        
+    public class UnitDatabaseData
+    {
         [field: SerializeField] public string ID { get; private set; }
         [field: SerializeField] public string DisplayName { get; private set; }
         [field: SerializeField] public Unit Prefab { get; private set; }
         [field: SerializeField] public float ProductionTime { get; private set; }
         [field: SerializeField] public int Cost { get; private set; }
         [field: SerializeField] public Resource.Type ResourceType { get; private set; }
+        [field: SerializeField] public int PoolingAmount { get; private set; } = 20;
     }
 }
