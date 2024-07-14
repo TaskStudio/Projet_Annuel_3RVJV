bbf55a56b9fd34a25fbdebf1913335499cefc25a
MrSebi06 <mihneasebastian06@gmail.com>
2024-07-14 20:29:21 +0200

    Setup for addressables

--- Assets/Scripts/Core/SaveSystem/PrefabInstantiator.cs
+++ Assets/Scripts/Core/SaveSystem/PrefabInstantiator.cs
@@ -1,13 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
-[System.Serializable]
-
-
+[Serializable]
 public class PrefabInstantiator : MonoBehaviour
 {
-    public TextAsset jsonFile; // Assign in the Inspector
-    void Start()
+    public TextAsset jsonFile;
+
+    private void Start()
     {
         ObjectList objectList = JsonUtility.FromJson<ObjectList>(jsonFile.text);
         Debug.Log(objectList.objects.Count);
@@ -19,13 +18,11 @@ public class PrefabInstantiator : MonoBehaviour
             string folderPath = objData.role == "unit" ? "Prefabs/Entities/" : "Prefabs/Construction/Buildings/";
             GameObject prefab = Resources.Load<GameObject>(folderPath + objData.type);
             if (prefab != null)
-            {
                 Instantiate(prefab, objData.position, objData.rotation);
-            }
             else
-            {
-                Debug.LogError($"Prefab not found for type '{objData.type}' at path: {folderPath + objData.type}. Ensure the prefab exists in a 'Resources' folder and the path is correct.");
-            }
+                Debug.LogError(
+                    $"Prefab not found for type '{objData.type}' at path: {folderPath + objData.type}. Ensure the prefab exists in a 'Resources' folder and the path is correct."
+                );
         }
     }
 }
