7e02d75a4a0bc91818e04a836d02281e09894b12
Onsraa <truongteddy306@icloud.com>
2024-07-16 20:41:36 +0200

    Add callback functions

--- Assets/Scripts/Managers/UI/SelectionMenuManager.cs
+++ Assets/Scripts/Managers/UI/SelectionMenuManager.cs
@@ -1,15 +1,18 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class SelectionMenuManager : MonoBehaviour
 {
     public UIDocument mainUIDocument;
+    public string mainSceneName = "MainScene";
 
     private void OnEnable()
     {
         var root = GetComponent<UIDocument>().rootVisualElement;
 
         var goBackButton = root.Q<Button>("GoBackButton");
+        var soloModeElement = root.Q<VisualElement>("SoloMode");
 
         if (goBackButton != null)
         {
@@ -19,15 +22,30 @@ public class SelectionMenuManager : MonoBehaviour
         {
             Debug.LogError("GoBackButton not found in the UXML document");
         }
+
+        if (soloModeElement != null)
+        {
+            soloModeElement.RegisterCallback<ClickEvent>(ev => LoadMainScene());
+        }
+        else
+        {
+            Debug.LogError("SoloMode element not found in the UXML document");
+        }
     }
 
     private void GoBackToMainMenu()
     {
-        // Hide credits UI
+        // Hide selection menu UI
         var root = GetComponent<UIDocument>().rootVisualElement;
         root.style.display = DisplayStyle.None;
 
         // Show main menu UI
         mainUIDocument.rootVisualElement.style.display = DisplayStyle.Flex;
+    }
+
+    private void LoadMainScene()
+    {
+        // Load the main scene
+        SceneManager.LoadScene(mainSceneName);
     }
 }
