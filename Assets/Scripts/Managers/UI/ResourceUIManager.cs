d23be0286cf87611b8dbc6560baf6def98c2c2b9
Onsraa <truongteddy306@icloud.com>
2024-07-18 01:21:18 +0200

    Add resource to UI

--- Assets/Scripts/Managers/UI/ResourceUIManager.cs
+++ Assets/Scripts/Managers/UI/ResourceUIManager.cs
@@ -6,10 +6,7 @@ public class ResourceUIManager : MonoBehaviour
 {
     public static ResourceUIManager Instance;
 
-    private static readonly Dictionary<Resource.Type, int> totalResources = new();
     private VisualElement resourcesContainer;
-    
-    public int startingWood;
 
     private void Awake()
     {
@@ -26,10 +23,6 @@ public class ResourceUIManager : MonoBehaviour
 
     private void Start()
     {
-        totalResources.Add(Resource.Type.Wood, startingWood);
-        totalResources.Add(Resource.Type.Stone, 0);
-        totalResources.Add(Resource.Type.Gold, 0);
-
         var rootVisualElement = GetComponent<UIDocument>().rootVisualElement;
         resourcesContainer = rootVisualElement.Q<VisualElement>("ResourcesContainer");
 
@@ -39,32 +32,18 @@ public class ResourceUIManager : MonoBehaviour
             return;
         }
 
-        UpdateResourceUI();
+        // Initial update based on ResourceManager data
+        UpdateResourceUI(ResourceManager.Instance.GetResources());
     }
 
-    public void UpdateResourceUI()
+    public void UpdateResourceUI(Dictionary<Resource.Type, int> resources)
     {
         resourcesContainer.Clear();
 
-        foreach (var resource in totalResources)
+        foreach (var resource in resources)
         {
             var resourceLabel = new Label { text = $"{resource.Key}: {resource.Value}" };
             resourcesContainer.Add(resourceLabel);
         }
-    }
-
-    public static void RegisterResource(Resource resource)
-    {
-        totalResources[resource.type] += resource.amount;
-        Instance.UpdateResourceUI();
-    }
-
-    public static bool RequestResource(Resource requestedResource)
-    {
-        if (totalResources[requestedResource.type] < requestedResource.amount) return false;
-
-        totalResources[requestedResource.type] -= requestedResource.amount;
-        Instance.UpdateResourceUI();
-        return true;
     }
 }
--- Assets/Scripts/Models/Gathering/ResourceManager.cs
+++ Assets/Scripts/Models/Gathering/ResourceManager.cs
@@ -5,6 +5,12 @@ public class ResourceManager : MonoBehaviour
 {
     public static ResourceManager Instance;
 
+    private Dictionary<Resource.Type, int> totalResources = new();
+    
+    public int startingWood;
+    public int startingStone;
+    public int startingGold;
+
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -18,13 +24,41 @@ public class ResourceManager : MonoBehaviour
         }
     }
 
-    public static void RegisterResource(Resource resource)
+    private void Start()
     {
-        ResourceUIManager.RegisterResource(resource);
+        InitializeResources();
+        ResourceUIManager.Instance.UpdateResourceUI(totalResources);
     }
 
-    public static bool RequestResource(Resource requestedResource)
+    private void InitializeResources()
     {
-        return ResourceUIManager.RequestResource(requestedResource);
+        totalResources.Add(Resource.Type.Wood, startingWood);
+        totalResources.Add(Resource.Type.Stone, startingStone);
+        totalResources.Add(Resource.Type.Gold, startingGold);
+    }
+
+    public void RegisterResource(Resource resource)
+    {
+        if (totalResources.ContainsKey(resource.type))
+        {
+            totalResources[resource.type] += resource.amount;
+            ResourceUIManager.Instance.UpdateResourceUI(totalResources);
+        }
+    }
+
+    public bool RequestResource(Resource requestedResource)
+    {
+        if (totalResources.ContainsKey(requestedResource.type) && totalResources[requestedResource.type] >= requestedResource.amount)
+        {
+            totalResources[requestedResource.type] -= requestedResource.amount;
+            ResourceUIManager.Instance.UpdateResourceUI(totalResources);
+            return true;
+        }
+        return false;
+    }
+
+    public Dictionary<Resource.Type, int> GetResources()
+    {
+        return new Dictionary<Resource.Type, int>(totalResources);
     }
 }
