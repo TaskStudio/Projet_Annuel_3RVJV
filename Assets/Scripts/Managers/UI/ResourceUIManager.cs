afce007329b00779235a6b462cb22dae05cf27fe
Onsraa <truongteddy306@icloud.com>
2024-07-19 20:32:55 +0200

    Modify Resource UI Manager script

--- Assets/Scripts/Managers/UI/ResourceUIManager.cs
+++ Assets/Scripts/Managers/UI/ResourceUIManager.cs
@@ -6,7 +6,9 @@ public class ResourceUIManager : MonoBehaviour
 {
     public static ResourceUIManager Instance;
 
-    private VisualElement resourcesContainer;
+    private Label woodQuantity;
+    private Label stoneQuantity;
+    private Label goldQuantity;
 
     private void Awake()
     {
@@ -24,11 +26,13 @@ public class ResourceUIManager : MonoBehaviour
     private void Start()
     {
         var rootVisualElement = GetComponent<UIDocument>().rootVisualElement;
-        resourcesContainer = rootVisualElement.Q<VisualElement>("ResourcesContainer");
+        woodQuantity = rootVisualElement.Q<Label>("WoodQuantity");
+        stoneQuantity = rootVisualElement.Q<Label>("StoneQuantity");
+        goldQuantity = rootVisualElement.Q<Label>("GoldQuantity");
 
-        if (resourcesContainer == null)
+        if (woodQuantity == null || stoneQuantity == null || goldQuantity == null)
         {
-            Debug.LogError("ResourcesContainer not found in the UXML. Check the UXML and the names.");
+            Debug.LogError("Labels not found in the UXML. Check the UXML and the names.");
             return;
         }
 
@@ -38,12 +42,20 @@ public class ResourceUIManager : MonoBehaviour
 
     public void UpdateResourceUI(Dictionary<Resource.Type, int> resources)
     {
-        resourcesContainer.Clear();
-
         foreach (var resource in resources)
         {
-            var resourceLabel = new Label { text = $"{resource.Key}: {resource.Value}" };
-            resourcesContainer.Add(resourceLabel);
+            switch (resource.Key)
+            {
+                case Resource.Type.Wood:
+                    woodQuantity.text = $"{resource.Value}";
+                    break;
+                case Resource.Type.Stone:
+                    stoneQuantity.text = $"{resource.Value}";
+                    break;
+                case Resource.Type.Gold:
+                    goldQuantity.text = $"{resource.Value}";
+                    break;
+            }
         }
     }
 }
