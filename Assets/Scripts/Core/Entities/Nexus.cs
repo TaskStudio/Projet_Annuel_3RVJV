c3a77b0864392c46f22b3c96f216057a6ac06ace
Onsraa <truongteddy306@icloud.com>
2024-07-21 23:01:50 +0200

    Debug Nexus to verify end game

--- Assets/Scripts/Core/Entities/Nexus.cs
+++ Assets/Scripts/Core/Entities/Nexus.cs
@@ -2,8 +2,15 @@ using UnityEngine;
 
 public class Nexus : Entity
 {
+    private void Update()
+    {
+        // Check if the spacebar is pressed
+        if (Input.GetKeyDown(KeyCode.Space)) TakeDamage(100);
+    }
+
     protected override void Die()
     {
         Debug.Log("Nexus is dead");
+        GameManager.Instance.OnNexusDestroyed(this);
     }
 }
